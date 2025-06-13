50683da7502405a213e1d5891073136864121367
gbc101579615 <genesis.tugawin@georgebrown.ca>
2025-06-13 16:36:50 -0400

    add get user details and display congratulation with score

--- Assignment1/Program.cs
+++ Assignment1/Program.cs
@@ -2,9 +2,26 @@
 {
     internal class Program
     {
+        static void GetUserDetails()
+        {
+            Console.WriteLine("Please enter your course: ");
+            string course = Console.ReadLine();
+            Console.WriteLine("Please enter your last name: ");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Please enter your first name: ");
+            string firstName = Console.ReadLine();
+        }
+
+        static void DisplayCongratulateScore(int currentScore)
+        {
+            Console.WriteLine("Congratulations you got it correct. SCORE: {0}",currentScore);
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            GetUserDetails();
+            DisplayCongratulateScore(99);
+            Console.ReadKey();
         }
     }
 }
--- Assignment1/obj/Debug/net8.0/Assignment1.AssemblyInfo.cs
+++ Assignment1/obj/Debug/net8.0/Assignment1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63e801ec9d0d3731d2f5c97ef80c78bdb8605527")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b50d0a4a114e9a5f6062e7d4c4eb6d1b93d48021")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
