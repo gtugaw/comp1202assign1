3584bb4a02df0d543106057df4acfa0b8b502d16
gbc101579615 <genesis.tugawin@georgebrown.ca>
2025-06-13 16:53:19 -0400

    rename project

--- Assignment1/obj/Debug/net8.0/Assignment1.AssemblyInfo.cs
+++ Assignment1/obj/Debug/net8.0/Assignment1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b50d0a4a114e9a5f6062e7d4c4eb6d1b93d48021")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa49e32ee625aea804399be7851b394c699b7b80")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
